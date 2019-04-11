60b3aa30ccbcb580b701274dd9acd35bfdbe04a4
rjmanzo <manzorenato@gmail.com>
2019-04-11 10:17:42 -0300

    Datasets, Requirements file & first Script

---
+++ rating-counter.py
@@ -0,0 +1,25 @@
+from pyspark import SparkConf, SparkContext
+import collections
+
+# set SparkConf and App name
+conf = SparkConf().setMaster("local").setAppName("RatingHistogram")
+# set SparkContext with the SparkConf info
+sc = SparkContext(conf = conf)
+
+# Read the lines from the archive
+lines = sc.textFile("../datasets/ml-100k/u.data")
+ratings = lines.map(lambda x: x.plit()[2])
+result = ratings.countByValue()
+
+sortedResults = collections.OrderedDict(sorted(result.items()))
+for key, value in sortedResults.items():
+    print("%s %i" % (key, value))
+
+
+
+
+
+
+
+
+
